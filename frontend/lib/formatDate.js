09e614210fccfc98033262d3d28865a9b6974915
Alex_Wang <binwcs.wang@mail.utoronto.ca>
2024-03-15 04:38:05 -0400

    Initial commit

---
+++ frontend/lib/formatDate.js
@@ -0,0 +1,7 @@
+
+export function formatDate(date) {
+    const date_input = new Date(date);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date_fmt = date_input.toLocaleDateString(undefined, options);
+    return date_fmt
+  }
---
+++ frontend/next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    BACKEND_URI: 'http://backend:8000'
+  },
+  webpack: (config, { isServer }) => {
+
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        // fs: false
+        fs: false,
+        path: false,
+        child_process: false,
+        crypto: false,
+        os: false,
+        tty: false
+      }
+    }
+
+    return config;
+  }
+};
